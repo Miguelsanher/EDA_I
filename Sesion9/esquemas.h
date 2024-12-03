5b2a79074ffae9851dcf778cc302da3848d2f0ca
Miguelsanher <20migsanher05@gnail.com>
2024-12-03 11:09:33 +0100

    restaurar

---
+++ Sesion9/esquemas.h
@@ -0,0 +1,35 @@
+/*
+ * esquemas.h	
+ *
+ *  Created on: 01/12/2014
+ *      Author: M José Polo
+ */
+
+#ifndef ESQUEMAS_H_INCLUDED
+#define ESQUEMA5_H_INCLUDED
+// Sesion 9: Divide y vencerás
+
+// Enunciado 1
+void Hanoi(int n,int origen, int destino, int aux);
+void HanoiERC(int n,int origen, int destino, int aux);
+// Enunciado 2
+int bTernaria(int *vector, int base, int tope,int x);
+// Enunciado 3
+long int potIterativa(long int a, int n); // Enuciado 6 tema 3.Análisis Algortimos
+long int potRecursiva(long int a, int n);// Enuciado 6 tema 3.Análisis Algortimos
+long int potDyV(long int a, int n); // Otra versión!!!
+
+#endif // ESQUEMAS_H_INCLUDED
+
+
+
+
+
+
+
+
+
+
+
+
+
